5b3c0a44ff134b86856a74e3f54b90a8cd8e31bf
QemerS <tu201806169@code.edu.az>
2021-10-26 03:12:42 +0400

    Add project files.

---
+++ BeshinciApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BeshinciApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program to find the sum of first 10 natural numbers
+            Console.WriteLine ("The first 10 natural numbers is:");
+            int sum = 0;
+            for (short x = 1; x <= 10; x++)
+            {
+                sum+= x;
+                Console.Write(x + " ");
+                if (x == 10) {
+                    Console.WriteLine("The sum is:" + sum );
+                }
+            }
+        }
+    }
+}
